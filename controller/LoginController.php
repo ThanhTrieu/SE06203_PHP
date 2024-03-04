c114257c693b66b19b3034c7241a40aa632754ba
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2024-03-04 20:23:14 +0700

    update code

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -9,10 +9,35 @@ switch($m){
     case 'index':
         index();
     break;
+    case 'handle':
+        handleLogin();
+    break;
     default:
         echo 'Not found request';
     break;
 }
+function handleLogin(){
+    // kiem tra nguoi bam submit login chua ?
+    if(isset($_POST['btnLogin'])){
+        // lay thong tin username
+        $username = trim($_POST['username'] ?? null);
+        $username = strip_tags($username);
+
+        // lay thong tin password
+        $password = trim($_POST['password'] ?? null);
+        $password = strip_tags($password);
+
+        if(empty($username) || empty($password)){
+            // nguoi dung ko nhap du thong tin
+            // quay ve lai trang login
+            header("Location:index.php?state=error");
+        } else {
+            // nguoi dung co nhap du thong tin
+        }
+    }
+}
 function index(){
-    echo "Hello you";
+    
+    // load view
+    require APP_PATH_VIEW . 'login/index_view.php';
 }
